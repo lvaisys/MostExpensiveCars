2538fad0c305969d12d4d20b630057d80cb068d1
MatasE0 <matase0@gmail.com>
2024-07-03 08:57:47 +0300

    removed bloat js

--- burgerButton.js
+++ burgerButton.js
@@ -4,11 +4,9 @@ const burgerNav = document.getElementById('burgerNav');
 burgerButton.addEventListener('click', () => {
     if (burgerNav.classList.contains('navHidden')) {
         burgerNav.classList.remove('navHidden');
-        void burgerNav.offsetWidth;
         burgerNav.classList.add('navVisible');
     } else {
         burgerNav.classList.remove('navVisible');
-        void burgerNav.offsetWidth;
         burgerNav.classList.add('navHidden');
 
     }
