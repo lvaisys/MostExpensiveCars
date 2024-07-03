036e396a2917b8f87880b6ee3063a0f344555ce7
MatasE0 <matase0@gmail.com>
2024-07-03 08:54:58 +0300

    burgeris animacija veikia kaskart paspaudus

--- burgerButton.js
+++ burgerButton.js
@@ -4,9 +4,11 @@ const burgerNav = document.getElementById('burgerNav');
 burgerButton.addEventListener('click', () => {
     if (burgerNav.classList.contains('navHidden')) {
         burgerNav.classList.remove('navHidden');
+        void burgerNav.offsetWidth;
         burgerNav.classList.add('navVisible');
     } else {
         burgerNav.classList.remove('navVisible');
+        void burgerNav.offsetWidth;
         burgerNav.classList.add('navHidden');
 
     }
