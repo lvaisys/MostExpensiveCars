1d756cfa06f5b7a189890487ffdc441993567025
MatasE0 <matase0@gmail.com>
2024-07-04 09:00:27 +0300

    no scrolling when nav is open

--- burgerButton.js
+++ burgerButton.js
@@ -1,10 +1,12 @@
 const burgerButton = document.getElementById('burgerButton');
 const burgerNav = document.getElementById('burgerNav');
 const navLinks = document.querySelectorAll('.aboutUsNav, .contactUsNav');
+const body = document.body;
 
 function toggleNav() {
   burgerNav.classList.toggle('navHidden');
   burgerNav.classList.toggle('navVisible');
+  body.classList.toggle('noScroll');
 }
 
 burgerButton.addEventListener('click', toggleNav);
